5dde4f53c118b0dfd7ed4250f52418d25f8077ab
Hans Ole Hatzel <hhatzel@gmail.com>
2017-02-13 17:40:09 +0100

    Improve handling of network errors.

--- client/main.py
+++ client/main.py
@@ -3,59 +3,74 @@ import requests
 import sys
 import time
 from collections import OrderedDict
+import threading
+from queue import Queue
 
 URL = "http://localhost:5000"
 ID = sys.argv[1]
 FULL_URL = "%s/%s" % (URL, ID)
 RELOAD_INTERVAL = 1
+_server_pop_queue = Queue()
+
 
 class Playlist:
+
     def __init__(self):
         self.played = set()
         self.playlist = OrderedDict()
 
     def update(self):
-        res = requests.get(FULL_URL)
-        print("Updating playlist form server")
-        new_items = [(vid["id"], vid["url"]) for vid in res.json()]
-        self.playlist.update(new_items)
+        try:
+            res = requests.get(FULL_URL)
+            print("Updating playlist form server")
+            new_items = [(vid["id"], vid["url"]) for vid in res.json()]
+            self.playlist.update(new_items)
+        except:
+            print("Can't connect to server :(")
+            return
 
     def set_one_played(self, name=None):
         """Registers one video as successfully played."""
         for id, url in self.playlist.items():
             if id not in self.played:
                 if name == url:
-                    print("Setting %s to 'played'" % name)
-                    # Todo: some queue for to pop items
-                    r = requests.get(FULL_URL + "/pop")
+                    _server_pop_queue.put(id)
                     self.played.add(id)
-                return
+                    return
 
     @property
     def not_played(self):
-        return [(k, url) for k, url in self.playlist.items() if k not in self.played]
+        return [(k, url) for k, url
+                in self.playlist.items() if k not in self.played]
 
     def update_mpv(self, player):
-        """Inserts tracks to mpv playlist until the instances total length - played tracks 
-        is equal the tracks to be played in MPV."""
-        print(self.played)
+        """Inserts tracks to mpv playlist until the instances total
+        length - played tracks is equal the tracks to be played in MPV."""
+        print("MPV UPdate")
         while True:
             tbp_mpv = to_be_played(player)
             tbp_list = len(self.playlist) - len(self.played)
-            print("mpv playlist still has %d to play while playlist class has %d" % (tbp_mpv, tbp_list))
+            print("To be played in mpv: %d" % tbp_mpv)
+            print("To be played in local: %d" % tbp_list)
             if tbp_mpv >= tbp_list:
                 break
             index = tbp_mpv
             url = self.not_played[index][1]
-            playlist_count = player._get_property("playlist-count", proptype=int)
-            playlist_current = player._get_property("playlist-pos", proptype=int)
+            playlist_count = player._get_property(
+                "playlist-count", proptype=int
+            )
+            playlist_current = player._get_property(
+                "playlist-pos", proptype=int
+            )
             percent = player._get_property("percent-pos", proptype=int) or 0
             if playlist_count == 0:
                 print("Playing %s now..." % url)
                 player.loadfile(url, "replace")
-            elif playlist_count == playlist_current + 1 and percent >= 99:
-                print("Append Playing %s now..." % url)
+            elif playlist_current is None or \
+                    (playlist_count == playlist_current + 1 and percent >= 99):
+                print("Appending and playing %s now..." % url)
                 player.loadfile(url, "append")
+                player.playlist_next()
                 player._set_property("pause", False, proptype=bool)
             else:
                 print("Appending %s to mpv playlist" % url)
@@ -65,6 +80,7 @@ class Playlist:
 def main():
     player = mpv.MPV(
         "osc",
+        "idle",
         ytdl=True,
         input_default_bindings=True,
         input_vo_keyboard=True,
@@ -73,14 +89,21 @@ def main():
         log_file="lol.log"
     )
     playlist = Playlist()
-    player.observe_property("percent-pos", lambda p: check_finished(p if p else 0, player, playlist))
-    player.observe_property("playlist-pos", lambda p: check_track_skip(p, player, playlist))
+    player.observe_property(
+        "percent-pos",
+        lambda p: check_finished(p if p else 0, player, playlist)
+    )
+    player.observe_property(
+        "playlist-pos",
+        lambda p: check_track_skip(p, player, playlist)
+    )
+    pop_thread = threading.Thread(target=pop_server, args=(_server_pop_queue,))
+    pop_thread.start()
     while True:
         playlist.update()
         playlist.update_mpv(player)
         time.sleep(RELOAD_INTERVAL)
     player.wait_for_playback()
-
 
 
 def to_be_played(player):
@@ -89,7 +112,7 @@ def to_be_played(player):
     percent = player._get_property("percent-pos", proptype=int) or 0
     if percent >= 99:
         pos += 1
-    return length - pos #length - (pos + 1)
+    return length - pos
 
 
 def check_track_skip(pos, player, playlist):
@@ -98,6 +121,21 @@ def check_track_skip(pos, player, playlist):
         old_pos = pos - 1
         name = player._get_property("playlist/%d/filename" % old_pos)
         playlist.set_one_played(name)
+
+
+def pop_server(queue):
+    while True:
+        id = queue.get()
+        try:
+            r = requests.post(FULL_URL + "/pop", json={"id": id})
+            if r.status_code != 200:
+                print("HTTP error %d" % r.status_code)
+            else:
+                print("Setting %s to 'played'" % id)
+        except requests.RequestException as e:
+            queue.put(id)
+            time.sleep(RELOAD_INTERVAL)
+        queue.task_done()
 
 
 def check_finished(percent, player, playlist):
